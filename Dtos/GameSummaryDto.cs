5087a280ed7ed9e162268e5ad80563ffed09a38e
Catalin Osman <113122931+catalinosman@users.noreply.github.com>
2024-03-26 18:29:23 +0200

    GameStore app

---
+++ Dtos/GameSummaryDto.cs
@@ -0,0 +1,9 @@
+﻿namespace GameStore.Api.Dtos;
+
+// records are immutable, the properties cannot change
+public record class GameSummaryDto(
+    int Id,
+    string Name,
+    string GenreId,
+    decimal Price,
+    DateOnly ReleaseDate);
---
+++ Endpoints/GamesEndpoints.cs
@@ -0,0 +1,82 @@
+﻿using GameStore.Api.Data;
+using GameStore.Api.Dtos;
+using GameStore.Api.Entities;
+using GameStore.Api.Mapping;
+using Microsoft.EntityFrameworkCore;
+
+namespace GameStore.Api.Endpoints;
+
+public static class GamesEndpoints
+{
+    const string GetGameEndpointName = "GetGame";
+
+    // a method that extends the functionality of an existing class
+    public static RouteGroupBuilder MapGamesEndpoints(this WebApplication app)
+    {
+        var group = app.MapGroup("games")
+        .WithParameterValidation();
+        // GET /games
+        group.MapGet("/", async (GameStoreDbContext dbContext) => 
+            await dbContext.Games
+                .Include(game => game.Genre)
+                .Select(game => game.ToGameSummaryDto())
+                .AsNoTracking()
+                .ToListAsync()) ;
+
+        // GET /games/1
+        group.MapGet("/{id}", async (int id, GameStoreDbContext dbContext) =>
+        {
+            Game? game = await dbContext.Games.FindAsync(id);
+
+            return game is null ?
+            Results.NotFound() : Results.Ok(game.ToGameDetailsDto());
+        })
+        .WithName(GetGameEndpointName);
+
+        // POST /games
+        group.MapPost("/", async (CreateGameDto newGame, GameStoreDbContext dbContext) => 
+        {
+            Game game = newGame.ToEntity();
+            
+
+            dbContext.Games.Add(game);
+            await dbContext.SaveChangesAsync();
+
+            return Results.CreatedAtRoute(
+                GetGameEndpointName,
+                new {id = game.Id},
+                game.ToGameDetailsDto());
+            // Results = built-in class in ASP.NET Core
+        })
+        .WithParameterValidation();
+
+        group.MapPut("/{id}", async (int id, UpdateGameDto updatedGame, GameStoreDbContext dbContext) => {
+            var existingGame = await dbContext.Games.FindAsync(id);
+
+            if (existingGame is null)
+            {
+                return Results.NotFound();
+            }
+
+            dbContext.Entry(existingGame)
+                .CurrentValues
+                .SetValues(updatedGame.ToEntity(id));
+
+            await dbContext.SaveChangesAsync();
+
+            return Results.NoContent();
+        });
+
+        // DELETE /games/1
+        group.MapDelete("/{id}", async (int id, GameStoreDbContext dbContext) => 
+        {
+            await dbContext.Games
+                .Where(game => game.Id == id)
+                .ExecuteDeleteAsync();
+        
+            return Results.NoContent();
+        });
+
+        return group;
+    }
+}
